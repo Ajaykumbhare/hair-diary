055dbc5077642908e94634f44e036b1bdc134d39
ajaykumbhare <ajaykumbhare@outlook.com>
2021-02-05 23:53:22 +0530

    hair-diary-implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Header } from './Components/Header';
+import { Tiles } from "./Components/Tiles";
+import GlobalContext from "./GlobalContext";
+import { fetchData } from "./fetch"
 
 function App() {
+  const [month, setMonth] = useState("Jan")
+  const [posts, setPosts] = useState({
+    "posts": []
+  });
+
+  useEffect(() => {
+    const reqObject = {
+      "requestobjects": [
+        {
+          "posts": {
+            "operationtype": "read",
+            "id": {
+              "return": true
+            },
+            "userid": {
+              "searchvalues": ["41329663-5834-11eb-8e6e-3ca82abc3dd4"],
+              "return": true
+            },
+            "iscalendarentry": {
+              "searchvalues": ["true"],
+              "return": true
+            },
+            "media": {
+              "return": true
+            },
+            "rating": {
+              "return": true
+            },
+            "text": {
+              "return": true
+            },
+            "privacy": {
+              "searchvalues": [
+                18
+              ],
+              "return": true
+            },
+            "typeofday": {
+              "return": true
+            },
+            "calendardatetime": {
+              "return": true,
+              "sort": "descending"
+            },
+            "maxitemcount": "50",
+            "continuationtoken": null
+          }
+        }
+      ]
+    }
+    fetchData(`https://devapi.quinn.care/graph`, "POST", JSON.stringify(reqObject))
+      .then((res) => res.responseobjects?.length > 0 && setPosts({
+        posts: res.responseobjects[0].posts
+      }))
+  }, [])
+
+
+  const changeMonth = (m: string) => setMonth(m);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalContext.Provider value={{ month, changeMonth, posts }}>
+      <div className="App">
+        <Header />
+        <Tiles />
+      </div>
+    </GlobalContext.Provider>
   );
 }
 
